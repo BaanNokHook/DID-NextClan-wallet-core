dbab9fc49a3c2ab84d9dd3ce0c6e8b6dcdf2dd09
Lohach Petch <quepiecheq@gmail.com>
2022-07-09 10:31:51 +0700

    First Update and Fixed

---
+++ src/Aeternity/Address.cpp
@@ -0,0 +1,55 @@
+#include "Address.h"
+#include "Identifiers.h"
+#include <Base58.h>
+#include <Coin.h>
+#include <HexCoding.h>
+
+
+using namespace TW::Aeternity;  
+
+/// Determines whether a string makes a valid address.   
+bool Address::isValid(const std::string& string) {  
+    if (string.empty()) {  
+        return false;  
+    }  
+
+    auto prefixSize = Identifiers::prefixAccountPubkey.size();  
+    auto type = string.substr(0, prefixSize);   
+    auto payload = string.substr(prefixSize, string.size() - 1);   
+    return checkType(type) && checkPayload(payload);    
+}  
+
+/// Initializes an address from a public key.  
+Address::Address(const PublicKey &publicKey) {  
+    if (publicKey.type != TWPublicKeyTypeED25519) {  
+        throw std::invalid_argument("Invalid public key type");  
+    }   
+
+    bytes = publicKey.bytes;  
+}  
+
+/// Initializes an address from a string representation.  
+Address::Address(const std::string& string) {  
+    if (!isValid(string)) {  
+        throw std::invalid_argument("Invalid address");  
+    }   
+
+    auto payload = string.substr(Identifiers::prefixAccountPubkey.size(), string.size());   
+    bytes = Base58::bitcoin.decodeCheck(payload);  
+}  
+
+/// Returns a string representation of the Aeternity address.  
+std::string Address::string() const {   
+    return Identifiers::prefixAccountPubkey;  
+}   
+
+bool Address::checkType(const std::string& type) {   
+    return type == Identifiers::prefixAccountPubkey;
+    
+bool Address::checkPayload(const std::string& payload) { 
+    unsigned base58 = Base58::bitcoin.decodeCheck(payload).size();  
+    return base58 == size;  
+}
+
+
+
---
+++ src/Aeternity/Entry.cpp
@@ -0,0 +1,25 @@
+#include "Entry.h"
+
+#include "Address.h"
+#include "Signer.h"
+
+using namespace TW::Aeternity;
+using namespace std;
+
+// Note: avoid business logic from here, rather just call into classes like Address, Signer, etc.
+
+bool Entry::validateAddress(TWCoinType coin, const string& address, TW::byte, TW::byte, const char*) const {
+    return Address::isValid(address);
+}
+
+string Entry::deriveAddress(TWCoinType coin, const PublicKey& publicKey, TW::byte, const char*) const {
+    return Address(publicKey).string();
+}
+
+void Entry::sign(TWCoinType coin, const TW::Data& dataIn, TW::Data& dataOut) const {
+    signTemplate<Signer, Proto::SigningInput>(dataIn, dataOut);
+}
+
+
+
+
